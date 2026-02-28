9fb70707d7077b00fcd1438042df6d4a1cbfeadb
elcuervo <elcuervo@elcuervo.net>
2026-02-27 22:13:56 -0300

    Add gitignore

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nestor - Network Share via TOR";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              curl
+            ];
+          };
+        });
+    };
+}
